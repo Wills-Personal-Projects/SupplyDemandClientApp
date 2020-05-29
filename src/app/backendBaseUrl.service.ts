3a6f6a569c7f875a3e8700962ffe9bc75d6fca95
freeWill <wjett18@gmail.com>
2020-05-29 15:28:40 -0700

    new backend service url

--- src/app/backendBaseUrl.service.ts
+++ src/app/backendBaseUrl.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class BackendBaseUrlService {
-  private baseUrl = 'http://supplydemandwebservice-env-6.eba-nutp8cwh.us-east-2.elasticbeanstalk.com/SupplyDemand/';
+  private baseUrl = 'http://supplydemandwebservice-env.eba-nutp8cwh.us-east-2.elasticbeanstalk.com/SupplyDemand/';
   getBaseUrl(){
     return this.baseUrl;
   }
